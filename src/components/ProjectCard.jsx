022533a70365f87282a2b88f562b3b41f67da14d
BirukW-23 <birukwondmeneh@gmail.com>
2026-02-24 10:28:55 +0300

    feat: project cards update

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -16,76 +16,6 @@ function ProjectCard() {
   const projects = [
     {
       id: 1,
-      image: image1,
-      title: "Top Movie Landing Page",
-      demoLink: "ethio-gym.netlify.app",
-      description:
-        "Is a dedicated webpage designed to promote an upcoming or released film. It provides essential details about the movie, engages visitors with visually appealing content, and encourages them to watch the film or stream it online.",
-      language: ["Html", "Css"],
-      color: "bg-emerald-500",
-    },
-    {
-      id: 2,
-      image: image2,
-      title: "Ethiopian Fitness Gym",
-      demoLink: "https://ethio-gym.netlify.app/",
-      description:
-        "A modern and responsive landing page designed for an Ethiopian fitness gym. The website highlights the gym’s services, class schedules, membership plans, and contact information with a clean layout and engaging visuals.",
-      language: ["Html", "Css", "JavaScript"],
-      color: "bg-cyan-500",
-    },
-    {
-      id: 3,
-      image: image3,
-      title: "Job Border Website",
-      demoLink: "ethio-gym.netlify.app",
-      description:
-        "A job board website is an online platform that connects job seekers with employers by listing job openings across various industries. It serves as a digital marketplace where employers can post job opportunities.",
-      language: ["React", "Tailwind", "Express", "PostgreSQL"],
-      color: "bg-rose-500",
-    },
-    {
-      id: 4,
-      image: image4,
-      title: "Agricultural support",
-      demoLink: "ethio-gym.netlify.app",
-      description:
-        "An agricultural support website is an online platform designed to assist farmers, agribusinesses, and agricultural professionals by providing resources, tools, and information to improve productivity.",
-      language: ["Html", "Bootstrap", "Php", "MySQL"],
-      color: "bg-orange-500",
-    },
-    {
-      id: 5,
-      image: image5,
-      title: "Crafted-BY-Her",
-      demoLink: "https://crafted-by-her.onrender.com/",
-      description:
-        "An e-commerce website for women to sell handmade products is a specialized online platform designed to empower female artisans, crafters, and entrepreneurs by providing them with a digital marketplace.",
-      language: ["React.js", "Tailwindcss", "Node.js", "Express.js", "MongoDb"],
-      color: "bg-pink-500",
-    },
-    {
-      id: 6,
-      image: image6,
-      title: "Gilbertin Website",
-      demoLink: "https://kansainvalinentyo.com/",
-      description:
-        "A website for visa consultancy services is an online platform designed to help individuals and businesses navigate the complex process of visa applications, immigration, and travel documentation.",
-      language: ["React.js", "Tailwindcss", "shadcn"],
-      color: "bg-blue-500",
-    },
-    {
-      id: 7,
-      image: image7,
-      title: "Yab Chemicals Website",
-      demoLink: "https://www.yabchemicals.com/",
-      description:
-        "A professional website for Yab Chemicals, a chemical manufacturing company. The site showcases company products, services, and industry expertise. It includes interactive sections upon product details.",
-      language: ["React", "TypeScript", "TailwindCSS"],
-      color: "bg-indigo-500",
-    },
-    {
-      id: 8,
       image: image8,
       title: "Rent Agreement System",
       demoLink: "https://aahrams.onrender.com/",
@@ -95,7 +25,7 @@ function ProjectCard() {
       color: "bg-purple-500",
     },
     {
-      id: 9,
+      id: 2,
       image: image9,
       title: "Rent Management System",
       demoLink: "https://act.com.et/",
@@ -103,6 +33,66 @@ function ProjectCard() {
         "Architected and developed a comprehensive rent management system for Atlas Computer Technology. System handles property listings, tenant management, payment processing, maintenance requests, and reporting.",
       language: ["Next.js", "Tailwind CSS", "Java", "Spring boot", "PostgreSQL"],
       color: "bg-teal-500",
+    },
+    {
+      id: 3,
+      image: image5,
+      title: "Crafted-BY-Her",
+      demoLink: "https://crafted-by-her.onrender.com/",
+      description:
+        "An e-commerce website for women to sell handmade products is a specialized online platform designed to empower female artisans, crafters, and entrepreneurs by providing them with a digital marketplace.",
+      language: ["React.js", "Tailwindcss", "Node.js", "Express.js", "MongoDb"],
+      color: "bg-pink-500",
+    },
+    {
+      id: 4,
+      image: image6,
+      title: "Gilbertin Website",
+      demoLink: "https://kansainvalinentyo.com/",
+      description:
+        "A website for visa consultancy services is an online platform designed to help individuals and businesses navigate the complex process of visa applications, immigration, and travel documentation.",
+      language: ["React.js", "Tailwindcss", "shadcn"],
+      color: "bg-blue-500",
+    },
+    {
+      id: 5,
+      image: image7,
+      title: "Yab Chemicals Website",
+      demoLink: "https://www.yabchemicals.com/",
+      description:
+        "A professional website for Yab Chemicals, a chemical manufacturing company. The site showcases company products, services, and industry expertise. It includes interactive sections upon product details.",
+      language: ["React", "TypeScript", "TailwindCSS"],
+      color: "bg-indigo-500",
+    },
+    {
+      id: 6,
+      image: image4,
+      title: "Agricultural support",
+      demoLink: "ethio-gym.netlify.app",
+      description:
+        "An agricultural support website is an online platform designed to assist farmers, agribusinesses, and agricultural professionals by providing resources, tools, and information to improve productivity.",
+      language: ["Html", "Bootstrap", "Php", "MySQL"],
+      color: "bg-orange-500",
+    },
+    {
+      id: 7,
+      image: image3,
+      title: "Job Border Website",
+      demoLink: "ethio-gym.netlify.app",
+      description:
+        "A job board website is an online platform that connects job seekers with employers by listing job openings across various industries. It serves as a digital marketplace where employers can post job opportunities.",
+      language: ["React", "Tailwind", "Express", "PostgreSQL"],
+      color: "bg-rose-500",
+    },
+    {
+      id: 8,
+      image: image2,
+      title: "Ethiopian Fitness Gym",
+      demoLink: "https://ethio-gym.netlify.app/",
+      description:
+        "A modern and responsive landing page designed for an Ethiopian fitness gym. The website highlights the gym’s services, class schedules, membership plans, and contact information with a clean layout and engaging visuals.",
+      language: ["Html", "Css", "JavaScript"],
+      color: "bg-cyan-500",
     },
   ];
 
